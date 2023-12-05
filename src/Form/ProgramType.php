a1079f0f478cd73b2fbb07242696c22780ea830b
THOMAS <thomaslunardo@gmail.com>
2023-12-05 17:20:48 +0100

    finish the quest 19

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Category;
 use App\Entity\Program;
+use App\Entity\Actor;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -22,7 +23,13 @@ class ProgramType extends AbstractType
         ->add('country', TextType::class)
         ->add('year', NumberType::class)
         ->add('category', EntityType::class, ['class' => Category::class, 'choice_label' => 'name']);
-        ;
+        
+        $builder->add('actors', EntityType::class, [
+            'class' => Actor::class,
+            'choice_label' => 'name',
+            'multiple' => true,
+            'expanded' => true,
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
